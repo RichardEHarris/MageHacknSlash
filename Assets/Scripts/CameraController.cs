8281c6b066ff8438df087ba389b21b633544ea60
Richard Harris <bananaxxbolt@gmail.com>
2020-06-08 01:15:22 +0930

    Added Basic UI
    Added Basic UI
    Started the player resource management (Health, mana and equipped spells)
    Added camera boundaries.
    Added props.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject cam;
     public GameObject player;
+    public Transform camBoundsLeft;
+    public Transform camBoundsRight;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,16 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 camPos = camera.transform.position;
+        Vector3 camPos = cam.transform.position;
         camPos.x = player.transform.position.x;
-        camera.transform.position = camPos;
+        if (camPos.x < camBoundsLeft.position.x)
+        {
+            camPos.x = camBoundsLeft.position.x;
+        }
+        if (camPos.x > camBoundsRight.position.x)
+        {
+            camPos.x = camBoundsRight.position.x;
+        }
+        cam.transform.position = camPos;
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    protected float health;
+    protected float mana;
+    protected Ability[] abilities = new Ability[4];
+    //TODO Add other stats and stuff
+}
